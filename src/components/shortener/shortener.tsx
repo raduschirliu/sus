7988ce2e6f296dcdf2d3ff0b3eba7507e7ac0425
Radu Schirliu <radu.schirliu@gmail.com>
2020-05-27 23:03:21 -0600

    feat: Added loading indicator while waiting for request

--- src/components/shortener/shortener.tsx
+++ src/components/shortener/shortener.tsx
@@ -1,40 +1,47 @@
-import React, { useState, ChangeEvent, ReactNode } from 'react';
-import { Button, TextField } from '@material-ui/core';
+import React, { useState, ChangeEvent } from 'react';
+import { Button, TextField, CircularProgress } from '@material-ui/core';
 import styles from './shortener.module.scss';
 import api from '../../api/api';
 import Link from '../../models/link';
 
+const LinkContent = ({ shortUrl, onReset }: { shortUrl: string; onReset: Function }) => {
+  if (shortUrl === '') return null;
+  const fullShortUrl = window.location + shortUrl
+
+  return (
+    <>
+      <h3>Your converted URL:</h3>
+      <a href={fullShortUrl}>{fullShortUrl}</a>
+      <Button color='primary' onClick={() => onReset()}>Clear</Button>
+    </>
+  );
+};
+
 const Shortener = () => {
+  const [loading, setLoading] = useState(false);
   const [url, setUrl] = useState('');
   const [shortUrl, setShortUrl] = useState('');
 
   const shorten = (): void => {
+    setLoading(true);
+
     api.shorten(url)
-      .then((res: any) => {
+      .then((res: Link) => {
         setShortUrl(res.id);
       })
       .catch(err => {
         setShortUrl("");
         console.error(err);
+      })
+      .finally(() => {
+        setLoading(false);
       });
   }
 
   const reset = (): void => {
     setShortUrl('');
     setUrl('');
-  }
-
-  const displayContent = (): ReactNode => {
-    if (shortUrl === '') return null;
-    const fullShortUrl = window.location + shortUrl
-
-    return (
-      <>
-        <h3>Your converted URL:</h3>
-        <a href={fullShortUrl}>{fullShortUrl}</a>
-        <Button color='primary' onClick={reset}>Clear</Button>
-      </>
-    );
+    setLoading(false);
   }
 
   return (
@@ -51,7 +58,11 @@ const Shortener = () => {
         <Button variant='contained' color='primary' onClick={shorten}>Shorten</Button>
 
         <div className={styles.urlResult}>
-          {displayContent()}
+          <div className={styles.loader}>
+            {loading && <CircularProgress />}
+          </div>
+          
+          <LinkContent shortUrl={shortUrl} onReset={reset} />
         </div>
       </div>
     </div>
